1b4df0f7918b012b87f9aad10f15c27b04b0a69b
yuchenyan <675917970@qq.com>
2018-04-16 22:17:59 +0800

    enemy modify

--- Enemy.java
+++ Enemy.java
@@ -1,4 +1,5 @@
-import greenfoot.*;
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
 /**
  * Write a description of class Enemy here.
  * 
@@ -7,14 +8,40 @@ import greenfoot.*;
  */
 public abstract class Enemy extends Actor
 {
-     int HP = 20;
-     int speed = 1;
-     int left;
-     int right;
-    public Enemy()
+    /**
+     * Act - do whatever the Enemy wants to do. This method is called whenever
+     * the 'Act' or 'Run' button gets pressed in the environment.
+     */
+    
+    public boolean live;
+    private boolean leftright=true;
+    private Scenario1 sce;
+    private double lastShot;
+    int HP =20;
+    int speed =1 ;
+    int left ;
+    int right;
+    public Enemy(Boolean live,Scenario1 sce)
     {
-        //todo enemy class
+        this.live = live;
+        this.sce=sce;
     }
     
+    public void act() 
+    {
+        // Add your action code here.
+        if ( System.currentTimeMillis()>lastShot+1500)  
+        {  
+            fire();  
+            lastShot = System.currentTimeMillis();
+        }  
+    }    
+    
+    private void fire(){
+         BulletEnemy bullet = new BulletEnemy(leftright,true);
+         sce.enemybullets.add(bullet);
+//         System.out.println(sce.bullets.size());
+         getWorld().addObject(bullet, getX(), getY());
+    }
     public abstract Enemy clone();
 }
